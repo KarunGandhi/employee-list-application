ebc6e517e5f57aea18659c01bbba07ebaa3232e8
KarunGandhi <karun.gandhi08@gmail.com>
2024-01-29 20:04:18 +0530

    final commit

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -6,21 +6,22 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class EmployeeService {
+  baseUrl = "http://localhost:3000";
   constructor(private _http: HttpClient) {}
 
   addEmployee(data: any): Observable<any> {
-    return this._http.post('http://localhost:3000/employees', data);
+    return this._http.post(`${this.baseUrl}/employees`, data);
   }
 
   updateEmployee(id: number, data: any): Observable<any> {
-    return this._http.put(`http://localhost:3000/employees/${id}`, data);
+    return this._http.put(`${this.baseUrl}/employees/${id}`, data);
   }
 
   getEmployeeList(): Observable<any> {
-    return this._http.get('http://localhost:3000/employees');
+    return this._http.get(`${this.baseUrl}/employees`);
   }
 
   deleteEmployee(id: number): Observable<any> {
-    return this._http.delete(`http://localhost:3000/employees/${id}`);
+    return this._http.delete(`${this.baseUrl}/employees/${id}`);
   }
 }
